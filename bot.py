1af36a701c79c8149a6565bdbdc8e19ba407a0e4
KamilaLis <kamila.lis@outlook.com>
2020-05-28 18:58:57 +0200

    cog

--- bot.py
+++ bot.py
@@ -7,42 +7,47 @@ from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-# GUILD = os.getenv()
 
-# bot = commands.Bot(command_prefix='*')
+bot = commands.Bot(command_prefix="*")
 
-class AmbientSound:
-    def __init__():
-        self.bot = commands.Bot(command_prefix="*")
 
-@bot.command(name='join')
-async def join(ctx):
-    user = ctx.message.author
-    guild = ctx.guild
-    voice_channel = user.voice.channel if user.voice else None
-    channel = None
-    if voice_channel != None:
-        await ctx.send(f"Connecting to {voice_channel.name}.")
-        self.vc = await voice_channel.connect()
-    else:
-        await ctx.send("You are not connected to any voice channel! I don't know where to go! :C")
+class AmbientSound(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.connected = False
+        # self.volume = bot.config.default_volume
 
-@bot.command(name='exit')
+    async def _play(self, audio):
+        self.vc.play(audio, after=lambda e: print("play", e))
 
-@bot.command(name='rain')
-async def rain(ctx):
-    response = "It rains!"
-    await ctx.send(response)
+    @commands.command(name='join')
+    async def join(self, ctx):
+        user = ctx.message.author
+        guild = ctx.guild
+        voice_channel = user.voice.channel if user.voice else None
+        channel = None
+        if voice_channel != None:
+            await ctx.send(f"Connecting to {voice_channel.name}.")
+            self.vc = await voice_channel.connect()
+            self.connected = True
+        else:
+            await ctx.send("You are not connected to any voice channel! I don't know where to go! :C")
 
-@bot.command(name='campfire')
-async def campfire(ctx):
-    
-        vc.play(FFmpegPCMAudio('campfire.mp3'), after=lambda e: print("coś", e))
-        while vc.is_playing():
-            await asyncio.sleep(1)
-        vc.stop()
-        await vc.disconnect()
-    else:
-        await ctx.send(user.name + " is not connected to any voice channel.")
+    @commands.command(name='exit')
+    async def exit(self, ctx):
+        await self.vc.disconnect()
 
+    @commands.command(name='rain')
+    async def rain(self, ctx):
+        response = "It rains!"
+        await ctx.send(response)
+
+    @commands.command(name='campfire')
+    async def campfire(self, ctx):
+        if not self.connected:
+            await self.play(FFmpegPCMAudio('campfire.mp3'))
+            print('test')
+
+
+bot.add_cog(AmbientSound(bot))
 bot.run(TOKEN)
